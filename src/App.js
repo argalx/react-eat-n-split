105583b51c5d6d1c19981010193bc653e97e6fa0
Arvin Bonggal <arvinbonggal@gmail.com>
2025-06-17 09:37:03 +0800

    Building the Static App: List of Friends

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+const initialFriends = [
+  {
+    id: 118836,
+    name: "Clark",
+    image: "https://i.pravatar.cc/48?u=118836",
+    balance: -7,
+  },
+  {
+    id: 933372,
+    name: "Sarah",
+    image: "https://i.pravatar.cc/48?u=933372",
+    balance: 20,
+  },
+  {
+    id: 499476,
+    name: "Anthony",
+    image: "https://i.pravatar.cc/48?u=499476",
+    balance: 0,
+  },
+];
 
-function App() {
+// Main component of the app
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="sidebar">
+        <FriendsList />
+      </div>
     </div>
   );
 }
 
-export default App;
+// Component to display the list of friends
+function FriendsList() {
+  const friends = initialFriends;
+
+  return (
+    <ul>
+      {/* // Map through the friends array and render each Friend component */}
+      {friends.map((friend) => (
+        <Friend key={friend.id} friend={friend} />
+      ))}
+    </ul>
+  );
+}
+
+// Component to display each friend's information
+function Friend({ friend }) {
+  return (
+    <li>
+      {/* // Display friend's image */}
+      <img src={friend.image} alt={friend.name} />
+
+      {/* // Display friend's name */}
+      <h3>{friend.name}</h3>
+
+      {/* // Display balance with different styles based on the value */}
+      {friend.balance < 0 && (
+        <p className="red">
+          You owe {friend.name} ${Math.abs(friend.balance)}
+        </p>
+      )}
+      {friend.balance > 0 && (
+        <p className="green">
+          {friend.name} owes you ${friend.balance}
+        </p>
+      )}
+      {friend.balance === 0 && <p>You and {friend.name} are even</p>}
+      <button className="button">Select</button>
+    </li>
+  );
+}
